156e17de4e6d6e6b35966fb111c1a10e0440d9c3
losewmac <losew@mail.ru>
2024-09-16 11:38:57 +0300

    homework4 is done

---
+++ src/main/kotlin/org/losewmac/kotlincourse/lesson4/homework4.kt
@@ -0,0 +1,124 @@
+package org.losewmac.kotlincourse.lesson4
+
+// Для каждого значения допиши подходящий тип данных
+
+//42
+val myInt1: Short = 42
+
+// 98765432123456789L
+val myLong1: Long = 98765432123456789L
+
+//23.45f
+val myFloat1: Float = 23.45f
+
+//0.123456789
+val myDouble1: Double = 0.123456789
+
+//"Kotlin & Java"
+val myText1: String = "Kotlin & Java"
+
+//false
+val myBoolean1: Boolean = false
+
+//'c'
+val myLetter1: Char = 'c'
+
+//500
+val myShort1: Int = 500
+
+//4294967296L
+val myLong2: Long = 4294967296L
+
+//18.0f
+val myFloat2: Float = 18.0f
+
+//-0.001
+val myDouble2: Double = -0.001
+
+//"OpenAI"
+val myText2: String = "OpenAI"
+
+//“true”
+val myText3: String = “true”
+
+//'9'
+val myLetter2: Char = '9'
+
+//2048
+val myShort2: Short = 2048
+
+//10000000000L
+val myLong3: Long = 10000000000L
+
+//5.75f
+val myFloat3: Float = 5.75f
+
+//1.414
+val myDouble3: Double = 1.414
+
+//"Artificial Intelligence"
+val myText4: String = "Artificial Intelligence"
+
+//false
+val myBoolean2: Boolean = false
+
+//'@'
+val myLetter3: Char = '@'
+
+//1024
+val myShort3: Short = 1024
+
+//1234567890123L
+val myLong4: Long = 1234567890123L
+
+//10.01f
+val myFloat4: Float = 10.01f
+
+//-273.15
+val myDouble4: Double = -273.15
+
+//"SpaceX"
+val myText4: String = "SpaceX"
+
+//true
+val myBoolean3: Boolean = true
+
+//“🤯”
+val myText6: String = “🤯”
+
+//‘65535’ - одинарные кавычки только для Char, правильно так
+val myText7: String = "65535"
+val myShort4: Short = 65535
+
+//72057594037927935L
+val myLong5: Long = 72057594037927935L
+
+//2.71828f
+val myFloat5: Float = 2.71828f
+
+//101.0101
+val myDouble5: Double = 101.0101
+
+//"Quantum Computing"
+val myText8: String = "Quantum Computing"
+
+//false
+val myBoolean4: Boolean = false
+
+//'x'
+val myLetter4: Char = 'x'
+
+//314
+val myShort5: Short = 314
+
+//123456789123456789L
+val myLong6: Long = 123456789123456789L
+
+//6.626f
+val myFloat6: Float = 6.626f
+
+//0.007
+val myDouble6: Double = 0.007
+
+//"Android Studio"
+val myText9: String = "Android Studio"
